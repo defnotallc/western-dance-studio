6fd5e2fd14bae1f724b80c59fc6a8bdbdd5d02e0
DefNotA <Defnota.official@gmail.com>
2026-04-13 18:37:29 -0500

    Initial Commit

---
+++ WesternDanceStudio/WesternDanceStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  WesternDanceStudioApp.swift
+//  WesternDanceStudio
+//
+//  Created by Jason Hamar on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct WesternDanceStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
